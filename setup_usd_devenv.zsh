bcd238f4362da93bd17b31f7b0fd3c25a83a0775
George ElKoura <c64kernal@gmail.com>
2016-08-25 21:06:05 -0700

    Adding USD imaging directories to cdpath.

--- setup_usd_devenv.zsh
+++ setup_usd_devenv.zsh
@@ -4,4 +4,4 @@ echo Setting up development environment variables...
 export T=$1
 export TD=$1
 
-cdpath=( $T $T/pxr/base/lib $T/pxr/usd/lib )
+cdpath=( $T $T/pxr/base/lib $T/pxr/usd/lib $T/pxr/imaging/lib $T/pxr/usdImaging/lib )
