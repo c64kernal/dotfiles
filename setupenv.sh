5da5382434c6012f5c0ea9435ec08cca57fc5c57
George ElKoura <c64kernal@gmail.com>
2019-03-09 11:27:38 -0800

    Added section for setting up git in the future

--- setupenv.sh
+++ setupenv.sh
@@ -13,7 +13,6 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
 
 
-
 ###############################################################################
 # Setup vim
 #
@@ -29,3 +28,12 @@ git clone https://github.com/vim-scripts/a.vim.git a-vim
 git clone https://github.com/quanganhdo/grb256.git
 git clone https://github.com/vim-airline/vim-airline-themes vim-airline
 popd
+
+
+###############################################################################
+# Setup git
+#
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    git config --global diff.tool opendiff
+    git config --global --add difftool.prompt false
+fi
